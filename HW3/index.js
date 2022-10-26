569fff06dc9bda283c137202bb727f5497504322
Ivan <vano553@ukr.net>
2022-10-26 21:12:57 +0300

    HW3

---
+++ HW3/index.js
@@ -0,0 +1,6 @@
+let userName = prompt("What is your name?");
+let userAge = prompt("How old are you?");
+let userHobbie = prompt("What is your favourite hobbie");
+alert(`Hello, ${userName}, you are ${userAge}, your favourite hobbie - ${userHobbie}`);
+const number = '12345'
+console.log(number[0], number[1], number[2], number[3], number[4])
